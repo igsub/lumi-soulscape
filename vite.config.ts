774ea1078a29fc86a531182394271c68c57c33b3
isuburuDB <ignacio.suburu@devbase.us>
2025-10-17 19:30:17 +0200

    fix deploy and local

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: '/lumi-soulscape/',
-})
+  base: command === 'build' ? '/lumi-soulscape/' : '/',
+}))
