996097963c10dbd0a467a7a17c9207315dc76219
isuburuDB <ignacio.suburu@devbase.us>
2025-10-17 20:41:05 +0200

    domain confing

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: command === 'build' ? '/lumi-soulscape/' : '/',
-}))
+  base: '/',
+})
